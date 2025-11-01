8e2445a8c77d1df4615694494646c91725e3a82b
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-11-01 15:44:38 +0300

    used small latters for files for letter case sensitive browsers and included the resume and the personal statement

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/App.css'; // or './App.css' if you used the default file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
